2f197f590bc31bc12b7626ae8efffeb3972ef7c8
Oleg <koe@sibdigital.net>
2025-01-21 17:04:11 +0800

    fix

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/main.tsx'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],

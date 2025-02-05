8a6089a264ac95741cc90f3e32fe78a37939f7c2
Oleg <koe@sibdigital.net>
2025-02-05 14:19:24 +0800

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@pages': '/src/pages',
+      '@modules': '/src/modules',
+      '@components': '/src/components',
+      '@ui': '/src/ui',
+      '@api': '/src/api',
+      '@services': '/src/services',
+      '@hooks': '/src/ui',
+    },
+  },
 })
